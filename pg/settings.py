0ea86f3e13f81e218ef584e421e652ad32f896c3
benguler <50894078+benguler@users.noreply.github.com>
2021-01-12 02:23:39 -0500

    Update settings.py

--- pg/settings.py
+++ pg/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = Path(BASE_DIR, 'root')
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     Path(BASE_DIR, 'static'),
