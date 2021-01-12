41d8596847a2a0ecc5a212e627ae1b565a733f86
benguler <50894078+benguler@users.noreply.github.com>
2021-01-12 02:47:34 -0500

    Update settings.py

--- pg/settings.py
+++ pg/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     Path(BASE_DIR, 'static'),
